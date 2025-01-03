f6e7d3cd4a1710e5c56e4a27b4c28631e39caad0
malavikavenugopal <malavikavenu914@gmail.com>
2025-01-03 12:18:27 +0530

    Updated code

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,15 +6,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCB2Do5TRsTLQNvg_y-sCwA6Rm4hU1zcns",
-  authDomain: "heroscoop-a628b.firebaseapp.com",
-  projectId: "heroscoop-a628b",
-  storageBucket: "heroscoop-a628b.firebasestorage.app",
-  messagingSenderId: "661021176654",
-  appId: "1:661021176654:web:a0113060822ee14650241f",
-  measurementId: "G-564Q4F1CCW"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
